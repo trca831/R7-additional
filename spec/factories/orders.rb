421188a98074857a88d024cc6a93dc4c1a6ed52c
Tracy Cano <tracycano3.14@gmail.com>
2024-01-14 17:55:26 -0800

    completed index file and spec models and requests for orders spec

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :order do
-    product_name { "MyString" }
-    product_count { 1 }
-    customer { nil }
+    product_name { Faker::Lorem.word }
+    product_count { Faker::Number.number(digits: 3).to_i }
+    association :customer
   end
 end
